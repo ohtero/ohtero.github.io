cad9795a16b7bad3c29976c0a84818f4bfca9758
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-28 18:13:42 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $("#background").fadeIn(5000, function(){
-    $("#entertext1").fadeIn(3000);
-    $("#entertext2").fadeIn(3000);
-    $("#entertext3").fadeIn(3000);
-    $("#entertext4").fadeIn(3000);
-    $("#entertext5").fadeIn(3000);
+
+  });
+});
+$("#entertext1").fadeIn(3000,function(){
+  $("#entertext1").fadeIn(3000,function(){
+    $("#entertext1").fadeIn(3000,function(){
+      $("#entertext1").fadeIn(3000,function(){
+        $("#entertext1").fadeIn(3000,function(){
+        });
+      });
+    });
   });
 });
