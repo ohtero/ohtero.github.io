011d986e8b8e82f60c3aedf976abbbe0bbdbfba1
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-28 22:48:10 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-  $(".hiddenimage").animate({opacity:1}, 5000, function(){
-    $("#entertext1").animate({opacity:1}, 2000, function(){
-      $("#entertext2").animate({opacity:1}, 2000, function(){
-        $("#entertext3").animate({opacity:1}, 2000,function(){
-          $("#entertext4").animate({opacity:1}, 2000, function(){
-            $("#entertext5").animate({opacity:1}, 2000, function(){
-              $("#entertext6").animate({opacity:1}, 1000, function(){
-                $("#enterLogo").animate({opacity:1}, 2000);
+    $(".hiddenimage").animate({opacity:1}, 5000, function(){
+      $("#enterLogo").animate({opacity:1}, 2000, function(){
+        $(#"enterLogo").animate({top: "500px"}, 1500, function(){
+          $("#entertext1").animate({opacity:1}, 2000, function(){
+            $("#entertext2").animate({opacity:1}, 2000, function(){
+              $("#entertext3").animate({opacity:1}, 2000,function(){
+                $("#entertext4").animate({opacity:1}, 2000, function(){
+                  $("#entertext5").animate({opacity:1}, 2000, function(){
+                    $("#entertext6").animate({opacity:1}, 1000);
+                  });
+                });
               });
             });
           });
@@ -14,4 +17,3 @@ $(document).ready(function(){
       });
     });
   });
-});
