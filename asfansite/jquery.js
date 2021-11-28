6410418574e61bcd2dd6c8da784d5829521cd48d
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-28 22:57:14 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".hiddenimage").animate({opacity:1}, 5000, function(){
       $("#enterLogo").animate({opacity:1}, 2000, function(){
-        $(#"enterLogo").animate({top: 500px}, 1500, function(){
+        $("#enterLogo").animate({top: 500}, 1500, function(){
           $("#entertext1").animate({opacity:1}, 2000, function(){
             $("#entertext2").animate({opacity:1}, 2000, function(){
               $("#entertext3").animate({opacity:1}, 2000,function(){
