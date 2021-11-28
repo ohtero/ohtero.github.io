0fb3672f14974eacde8852938e6689ffaab977e9
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-28 20:31:21 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $(".hiddenimage").animate({opacity:1}, 5000function(){
+  $(".hiddenimage").animate({opacity:1}, 5000, function(){
     $("#entertext1").fadeIn(3000, function(){
       $("#entertext2").fadeIn(3000, function(){
         $("#entertext3").fadeIn(3000,function(){
           $("#entertext4").fadeIn(3000, function(){
             $("#entertext5").fadeIn(3000, function(){
-              $("#entertext6").fadeIn(3000);  
+              $("#entertext6").fadeIn(3000);
             });
           });
         });
