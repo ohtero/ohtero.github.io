89e0cd23351b4a65bd0f6dd3adf90f7d0d5b10e6
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-29 17:39:47 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".hiddenimage").animate({opacity:1}, 5000, function(){
       $("#enterLogo").animate({opacity:1}, 2000, function(){
-        $("#enterLogo").animate({bottom: "110%"}, 1500, function(){
+        $("#enterLogo").animate({bottom: "100%"}, 1500, function(){
           $("#entertext1").animate({opacity:1}, 2000, function(){
             $("#entertext2").animate({opacity:1}, 2000, function(){
               $("#entertext3").animate({opacity:1}, 2000,function(){
