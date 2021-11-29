988c332f5fd25139bc53922b997d86c51a8a2a0e
Tuhto <91533120+Tuhto@users.noreply.github.com>
2021-11-29 23:10:08 +0200

    asd

--- asfansite/jquery.js
+++ asfansite/jquery.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
               $("#entertext3").animate({opacity:1}, 2000,function(){
                 $("#entertext4").animate({opacity:1}, 2000, function(){
                   $("#entertext5").animate({opacity:1}, 2000, function(){
-                    $("#entertext6").animate({opacity:1}, 1000);
+                    $("#entertext6").animate({opacity:1}, 1000, function(){
+                      $("#enterButton").animate({opacity:1}, 3000);
+                    });
                   });
                 });
               });
